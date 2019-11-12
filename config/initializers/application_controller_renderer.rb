6e8dee99ec4ce3947026fe8c098e46e9396b339f
kashio-tech <gumgum.yk.29@gmail.com>
2019-11-12 21:02:17 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3982a79573b77bcaebd390dd80519444ce37a97abe13feef868c805be3148986ec944bce407d3e29e8d374b029586b223761b12324701ce3fa5181a052d9c1ee'
